1cefe21b1fd5a172d6c93ad394943ba7c46ffbdb
nitishrestha <nitishrestha8848@gmail.com>
2023-04-23 21:50:32 +0545

    Touchable highlight with multiple buttons

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 const App = () => {
   return (
     <View>
-      <Text style={styles.title}>Main Page</Text>
+      <Text style={styles.title}>Touchable highlight n opacity</Text>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.delete]}>Delete</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.primary]}>Primary</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.success]}>Success</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.warn]}>Warn</Text>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Text style={[styles.button, styles.error]}>Error</Text>
+      </TouchableHighlight>
     </View>
   );
 };
@@ -11,11 +32,31 @@ const App = () => {
 const styles = StyleSheet.create({
   title: {
     marginTop: 45,
-    fontSize: 40,
-    color: "white",
+    fontSize: 25,
+    height: 70,
     textAlign: "center",
+    color: "white",
+    paddingTop: 15,
     backgroundColor: "#bd5734",
   },
+  button: {
+    backgroundColor: "#87bdd8",
+    color: "white",
+    height: 50,
+    fontSize: 20,
+    padding: 10,
+    textAlign: "center",
+    margin: 15,
+    borderRadius: 10,
+    shadowColor: "red",
+    elevation: 10,
+    shadowOpacity: 0.5,
+  },
+  delete: { backgroundColor: "#c83349", color: "white" },
+  primary: { backgroundColor: "#b8a9c9", color: "white" },
+  success: { backgroundColor: "#96ceb4", color: "white" },
+  warn: { backgroundColor: "#d96459", color: "white" },
+  error: { backgroundColor: "#622569", color: "white" },
 });
 
 export default App;
