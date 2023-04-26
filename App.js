6b772fd732c5f70bff60c286a947efd52d7b7b7c
nitishrestha <nitishrestha8848@gmail.com>
2023-04-26 22:34:57 +0545

    created custom radio button

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const App = () => {
+  const [selectedRadio, setSelectedRadio] = useState(1);
   return (
-    <View>
-      <Text style={styles.title}>Radio Button</Text>
+    <View style={{ flex: 1 }}>
+      <View>
+        <Text style={styles.title}>Radio Button</Text>
+      </View>
+      <View style={styles.main}>
+        <TouchableOpacity onPress={() => setSelectedRadio(1)}>
+          <View style={styles.radioWrapper}>
+            <View style={styles.radio}>
+              {selectedRadio === 1 ? (
+                <View style={styles.radioBG}></View>
+              ) : null}
+            </View>
+            <Text style={styles.radioText}>Radio 1</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSelectedRadio(2)}>
+          <View style={styles.radioWrapper}>
+            <View style={styles.radio}>
+              {selectedRadio === 2 ? (
+                <View style={styles.radioBG}></View>
+              ) : null}
+            </View>
+            <Text style={styles.radioText}>Radio 2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,6 +41,30 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     backgroundColor: "#bd5734",
+  },
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  radioText: { fontSize: 20 },
+  radio: {
+    height: 40,
+    width: 40,
+    borderColor: "pink",
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10,
+  },
+  radioWrapper: { flexDirection: "row", alignItems: "center" },
+  radioBG: {
+    height: 28,
+    width: 28,
+    borderWidth: 2,
+    backgroundColor: "pink",
+    borderColor: "pink",
+    margin: 4,
+    borderRadius: 20,
   },
 });
 
