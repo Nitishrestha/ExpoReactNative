5c0b6dbc8ca646fa891af888b494e8485ea5d456
nitishrestha <nitishrestha8848@gmail.com>
2023-04-26 23:16:59 +0545

    activity indicator

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const App = () => {
+  const [show, setShow] = useState(false);
+
+  const displayLoader = () => {
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text style={styles.title}>Main Page</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={styles.title}>Activity Indicator</Text>
+      <View style={styles.loading}>
+        <ActivityIndicator
+          size={90}
+          color={"#bd5734"}
+          animating={show}
+        ></ActivityIndicator>
+        <Button title="Show loader" onPress={() => displayLoader()}></Button>
+      </View>
     </View>
   );
 };
@@ -15,6 +40,11 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     backgroundColor: "#bd5734",
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
