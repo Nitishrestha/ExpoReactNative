d36f4ffc590e8b749adec6365fe2bdaefc26a80e
nitishrestha <nitishrestha8848@gmail.com>
2023-04-27 21:33:27 +0545

    Created modal

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, View, Modal } from "react-native";
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
-    <View>
-      <Text style={styles.title}>Main Page</Text>
+    <View style={styles.mainView}>
+      <Text style={styles.title}>Dialog box in react native</Text>
+
+      <Modal transparent={true} visible={show} animationType="slide">
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello All</Text>
+            <Button title="Close Modal" onPress={() => setShow(false)} />
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.buttonView}>
+        <Button title="Open Modal" onPress={() => setShow(true)}></Button>
+      </View>
     </View>
   );
 };
@@ -11,11 +26,26 @@ const App = () => {
 const styles = StyleSheet.create({
   title: {
     marginTop: 45,
-    fontSize: 40,
+    fontSize: 30,
     color: "white",
     textAlign: "center",
     backgroundColor: "#bd5734",
   },
+  mainView: { flex: 1 },
+  buttonView: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  centeredView: { flex: 1, justifyContent: "center", alignItems: "center" },
+  modalView: {
+    backgroundColor: "red",
+    padding: 30,
+    borderRadius: 20,
+    shadowColor: "black",
+    elevation: 5,
+    marginTop: 100,
+  },
+  modalText: { fontSize: 20, marginBottom: 20 },
 });
 
 export default App;
