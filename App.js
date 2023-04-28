6c4070e2501bcc2831756760eae0e5abe36d8de0
nitishrestha <nitishrestha8848@gmail.com>
2023-04-28 21:23:43 +0545

    commit pressable

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.title}>Main Page</Text>
+    <View style={{ flex: 1 }}>
+      <View>
+        <Text style={styles.title}>Pressable</Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Pressable
+          onPress={() => console.warn("On normal press")}
+          onLongPress={() => console.warn("on long press")}
+          onPressIn={() => console.warn("on press in")}
+          onPressOut={() => console.warn("on press out")}
+        >
+          <Text style={styles.pressableBtn}>Press</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -15,6 +27,18 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     backgroundColor: "#bd5734",
+  },
+  pressableBtn: {
+    alignContent: "center",
+    backgroundColor: "blue",
+    color: "white",
+    padding: 10,
+    margin: 10,
+    fontSize: 20,
+    textAlign: "center",
+    borderRadius: 20,
+    shadowColor: "black",
+    elevation: 5,
   },
 });
 
