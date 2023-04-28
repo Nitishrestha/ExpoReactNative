5da369984a0bccff893de67ed2925122130f3486
nitishrestha <nitishrestha8848@gmail.com>
2023-04-28 22:11:54 +0545

    react web view

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import WebView from "react-native-webview";
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.title}>Main Page</Text>
+    <View style={styles.title}>
+      <WebView
+        source={{ uri: "https://reactnative.dev/docs/next/pressable" }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   title: {
+    flex: 1,
     marginTop: 40,
     fontSize: 40,
     color: "white",
