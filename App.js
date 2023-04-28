44876dbc64da2086b1ff6f6eb2350d742c9d4105
nitishrestha <nitishrestha8848@gmail.com>
2023-04-28 21:45:59 +0545

    set status bar color, set the bar styles

--- App.js
+++ App.js
@@ -1,9 +1,42 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const App = () => {
+  const [show, setShow] = useState(false);
+  const [barStyle, setbarStyle] = useState("default");
+
   return (
-    <View>
-      <Text style={styles.title}>Status Bar</Text>
+    <View style={{ flex: 1 }}>
+      <View>
+        <Text style={styles.title}>Status Bar</Text>
+      </View>
+      <View>
+        <StatusBar
+          backgroundColor={"lightgreen"}
+          barStyle={barStyle}
+          hidden={show}
+        ></StatusBar>
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", margin: 20 }}>
+        <Button title="Toggle" onPress={() => setShow(!show)}></Button>
+
+        <Button
+          title="Update to dark"
+          onPress={() => setbarStyle("dark-content")}
+        ></Button>
+
+        <Button
+          title="Update to light"
+          onPress={() => setbarStyle("light-content")}
+        ></Button>
+      </View>
     </View>
   );
 };
