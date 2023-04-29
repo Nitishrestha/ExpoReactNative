3a5447ad2ac30229e0d4905d8324e444b98c256b
nitishrestha <nitishrestha8848@gmail.com>
2023-04-29 18:06:31 +0545

    Implement bottom navigation

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 const App = () => {
   return (
-    <View>
-      <Text style={styles.title}>Main Page</Text>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="SignUp" component={SignUp} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Login = () => {
+  return (
+    <View style={styles.login}>
+      <Text style={styles.text}>Login </Text>
     </View>
   );
 };
 
+const SignUp = () => {
+  return (
+    <View style={styles.signup}>
+      <Text style={styles.text}>SignUp </Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   title: {
     marginTop: 40,
@@ -16,6 +38,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     backgroundColor: "#bd5734",
   },
+  signup: { flex: 1, justifyContent: "center", alignItems: "center" },
+  login: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 40, textAlign: "center" },
 });
 
 export default App;
